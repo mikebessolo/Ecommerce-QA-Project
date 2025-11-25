ce7e25d95dc414c9fca5eb2b13767ee3cc3269fd
mikebessolo <abessolomichael@gmail.com>
2025-11-25 00:26:34 -0500

    Update Login_page.py

--- Login_page.py
+++ Login_page.py
@@ -1,5 +1,3 @@
-Python 3.12.3 (v3.12.3:f6650f9ad7, Apr  9 2024, 08:18:47) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
 >>> from selenium.webdriver.common.by import By
 ... 
 ... class LoginPage:
